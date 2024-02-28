747c55b228a2220e07a5156512f1e926cba2cffc
peterodero561 <peterodero561@gmail.com>
2024-02-28 14:27:34 +0000

    feat: added code to assign left child parent to new if the parent left child is not empty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (parent == NULL)
+	if (parent == NULL || value == '\0')
 		return (NULL);
 
 	new = malloc(sizeof(binary_tree_t));
@@ -23,11 +23,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->n = value;
 	new->parent = parent;
 	new->right = NULL;
+	new->left = NULL;
 	if (parent->left == NULL)
 		parent->left = new;
 	else
 	{
 		new->left = parent->left;
+		new->left->parent = new;
 		parent->left = new;
 	}
 	return (new);
