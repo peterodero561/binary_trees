01b051d1ac6073fc8da0cee465618feaea2f1591
peterodero561 <peterodero561@gmail.com>
2024-02-28 14:40:25 +0000

    fix: corrected betty errors

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left child of
+ * binary_tree_insert_right - inserts a node as the left child of
  * another node
  * @parent: is a pointer to the node to insert the left child in
  * @value: is the value to store in the new node
