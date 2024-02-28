0c4398b19fa38c9ad4be8c69da32305a6707febf
peterodero561 <peterodero561@gmail.com>
2024-02-28 19:31:18 +0000

    fix: removed unsed declared variables and typo in return statement

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,9 +8,7 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
-
 	if (tree == NULL)
 		return (0);
-	return (1 + binary_tree_size(tree->left) + binary_tree(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
