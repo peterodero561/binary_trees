36d50a93ad493ad340e5ff66bf74d25299a69f4b
peterodero561 <peterodero561@gmail.com>
2024-02-29 04:14:25 +0000

    feat: aded code to consider a tree with only a child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,10 +14,14 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL || tree->left != NULL)
-		return (1);
+	{
+		lr = binary_tree_nodes(tree->right);
+		ll = binary_tree_nodes(tree->left);
 
-	lr = binary_tree_nodes(tree->right);
-	ll = binary_tree_nodes(tree->left);
-
-	return (ll + lr);
+		return (ll + lr + 1);
+	}
+	else
+	{
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	}
 }
