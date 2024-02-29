a9a68628c46d47885609474fb9654fdc3662bc75
peterodero561 <peterodero561@gmail.com>
2024-02-29 12:40:53 +0000

    perf: new approach to the problem

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -10,11 +10,38 @@
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
+	/**
+	 * if (tree == NULL || func == NULL)
+	 * return;
+	 * func(tree->n);
+	 * binary_tree_levelorder(tree->left, func);
+	 * func(tree->left->n);
+	 * binary_tree_levelorder(tree->right, func);
+	 * func(tree->right->n);
+	 */
+
 	if (tree == NULL || func == NULL)
 		return;
-	func(tree->n);
-	binary_tree_levelorder(tree->left, func);
-	func(tree->left->n);
-	binary_tree_levelorder(tree->right, func);
-	func(tree->right->n);
+
+	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1024);
+
+	if (queue == NULL)
+		return;
+
+	int front = 0, rear = 0;
+
+	queue[rear++] = (binary_tree_t *)tree;
+	while (front < rear)
+	{
+		const binary_tree_t *current = queue[front++];
+
+		func(current->n);
+
+		if (current->left != NULL)
+			queue[rear++] = current->left;
+
+		if (current->right != NULL)
+			queue[rear++] = current->right;
+	}
+	free(queue);
 }
