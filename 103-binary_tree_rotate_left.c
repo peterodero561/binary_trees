51885bb00379b9d5a39b2252d323174ec692d58d
peterodero561 <peterodero561@gmail.com>
2024-02-29 14:19:51 +0000

    new

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -5,20 +5,20 @@
  * @tree: pointer to the root node of the tree to rotate
  * Return: pointer to the new root node of the tree once rotated
  */
-
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	if (tree == NULL || tree->right == NULL)
-	return (NULL);
+		return (NULL);
 
 	binary_tree_t *new_root = tree->right;
+	binary_tree_t *temp = new_root->left;
 
-	tree->right = new_root->left;
-
-	if (new_root->left != NULL)
-		new_root->left->parent = tree;
 	new_root->left = tree;
+	tree->right = temp;
+	if (temp != NULL)
+		temp->parent = tree;
 	new_root->parent = tree->parent;
 	tree->parent = new_root;
 	return (new_root);
 }
+
